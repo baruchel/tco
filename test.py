a6c3421ca91ea92d71f020b0c7b075eece3747c9
Thomas Baruchel <baruchel@gmx.com>
2015-07-12 15:51:26 +0200

    Update

---
+++ test.py
@@ -0,0 +1,29 @@
+from tco import C2 as C
+import traceback
+import time
+
+# tests
+def display(x):
+  print((len(traceback.extract_stack()),x))
+def display1(x,y):
+  print(("ok",len(traceback.extract_stack()),(x,y)))
+def display2(x,y):
+  print(("err",len(traceback.extract_stack()),(x,y)))
+
+disp  = C(lambda f: lambda x: display(x))()
+disp1 = C(lambda f: lambda x,y: display1(x,y))()
+disp2 = C(lambda f: lambda x,y: display2(x,y))()
+
+t = C( lambda f, k1, k2: lambda n:
+           C(lambda f: lambda i,j: f(i-1,not j) if i>0
+                else k1(42,0) if j else k2(42,0))()(n,True) )
+
+t(disp1,disp2)(1000)
+
+f = C(
+       lambda self, d:
+         lambda n:
+           self(n-1) if n>0 else d(42)
+      )(disp)
+t = time.time(); f(5000); print(time.time()-t)
+    
