fc7526cbc5c25a9c821bc163ac0c8eac21cbfaa1
Thomas Baruchel <baruchel@gmx.com>
2015-07-12 15:53:48 +0200

    Update

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # tco
 Tail Call Optimization for Python
+
+A module for performing tail-call optimization in Python code. Compatible with Python 2 and Python 3.
+
+The module allows a coder to write tail-recursive functions as well as using continuation-passing style in his/her code without having the size of the call stack increasing.
+
+The module contains two different implementations of a trampoline-based wrapper for tail-call optimized functions. Only one should be imported according to the needs; the following `import` statements will allow to use the very same syntax in the following code:
+
+    from tcp import C
+    from tco import C2 as C
+
+The first wrapper is a little faster and should be used unless specific needs occur; the most important thing to remember concerning this wrapper is: *the tail-optimized function may note return any callable object as its final result*.
+
+The second wrapper is more robust but a little slower; the most important thing to know about it is that *intermediate functions may not catch the internal exception used by the wrapper* (catching other exceptions is allowed however).
+
+The syntax for using the wrapper is:
+
+    f = C(
+           lambda self, k1, k2, k3, ...:
+             lambda *args:
+               ... code here ...
+          )
+
+where the argument `self` will be used for referring to the function itself (recursion case) and any number (including none) of other continuations `k1`, `k2`, etc. can be used as well.
+
+Using the function is done as:
+
+    f( k1, k2, k3, ...) ( *args )
+
+where the self reference is implicit (only other continuations have to be provided). Functions used as continuations should of course be created with the `C` wrapper also.
+
+A tail-recursive version of the factorial function (using an accumulator) is:
+
+    fac = C(
+             lambda self:
+               lambda n, acc:
+                 self(n-1,n*acc) if n>1 else acc
+            )()
+    print( fac(5,1) )
+
+The file `test.py` gives some other examples.
--- tco.py
+++ tco.py
@@ -2,6 +2,33 @@
 Allow to use tail-call optimized functions in Python code (for tail-recursion or continuation-passing style).
 """
 
+class _TailCallWrapper():
+    """
+    Wrapper for tail-called optimized functions embedding their continuations.
+    Such functions are ready to be evaluated with their arguments.
+
+    This is a private class and should never be accessed directly.
+    Functions should be created by using the C() class first.
+    """
+    def __init__(self,func, k):
+        self.func = func(
+          lambda *args: lambda: self.func(*args),
+          *map( lambda c: lambda *args: lambda: c.func(*args), k) )
+    def __call__(self, *args):
+        f = lambda: self.func(*args)
+        while callable(f):
+            f = f()
+        return f
+
+class C():
+    """
+    Main wrapper for tail-call optimized functions.
+    """
+    def __init__(self, func):
+        self.func = func
+    def __call__(self, *k):
+        return _TailCallWrapper(self.func, k)
+
 class _TailCall(Exception):
     def __init__(self, f, args):
         self.func = f.func
@@ -12,7 +39,7 @@ def _tailCallback(f):
         raise _TailCall(f,args)
     return t
 
-class _TailCallWrapper():
+class _TailCallWrapper2():
     """
     Wrapper for tail-called optimized functions embedding their continuations.
     Such functions are ready to be evaluated with their arguments.
@@ -32,52 +59,11 @@ class _TailCallWrapper():
                 f = e.func
                 args = e.args
         
-class C():
+class C2():
     """
     Main wrapper for tail-call optimized functions.
-
-    Usage:
-        f = C(
-               lambda self [,k1, k2, ...]:
-                 lambda *args:
-             )
-       where 'self' is intended for a recursive call
-       and k1, k2, etc. are other contiuations also defined with C()
-
-       Invoking 'f' is done as:
-         f( [k1, k2, ...] )(*args)
-       where the 'self' continuation is implicit.
-
-    Examples:
-      # Factorial function
-      fac = C(
-               lambda self:
-                 lambda n, acc:
-                   self(n-1,n*acc) if n>1 else acc
-              )()
-      print( fac(5,1) )
     """
     def __init__(self, func):
         self.func = func
     def __call__(self, *k):
-        return _TailCallWrapper(self.func, k)
-
-
-
-
-# tests
-# import traceback
-# def display1(x,y):
-#   print(("ok",len(traceback.extract_stack()),(x,y)))
-# def display2(x,y):
-#   print(("err",len(traceback.extract_stack()),(x,y)))
-# 
-# disp1 = C(lambda f: lambda x,y: display1(x,y))()
-# disp2 = C(lambda f: lambda x,y: display2(x,y))()
-# 
-# Y = lambda f: (lambda x: x(x))(lambda y: f(lambda *args: y(y)(*args)))
-# test = ( lambda f, k1, k2: lambda n:
-#            Y(lambda f: lambda i,j: f(i-1,not j) if i>0
-#                 else k1(42,0) if j else k2(42,0))(n,False) )
-# 
-# C(test)(disp1,disp2)(150)
+        return _TailCallWrapper2(self.func, k)
