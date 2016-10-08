5120c96c4e8be66e498e53df1f564fa5400f01de
Thomas Baruchel <baruchel@gmx.com>
2016-10-08 21:01:27 +0200

    Changed to __init__.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+pythfrom setuptools import setup
+
+setup(
+        name='tco',
+        packages=['tco'],
+        description='Tail Call Optimization for Python',
+        license='MIT',
+        author='Thomas Baruchel',
+        author_email = 'baruchel@gmx.com',
+        version='1.1.1',
+        url='https://github.com/baruchel/tco'
+)
